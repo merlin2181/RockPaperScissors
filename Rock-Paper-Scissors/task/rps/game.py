d13884c7339c11c5b789d9745f2ef06d61ef2e79
merlin2181 <merlin2181@gmail.com>
2020-07-04 14:03:25 -0700

    Stage 2 of Rock Paper Scissors project from JetBrains Academy

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,13 +1,20 @@
 # Write your code here
-def comp_wins(player):
-    if player == "scissors":
-        return "rock"
-    if player == "paper":
-        return "scissors"
-    if player == "rock":
-        return "paper"
+import random
 
 
-user_choice = input()
-computer = comp_wins(user_choice)
-print(f"Sorry, but computer chose {computer}")
+def winner(player, computer):
+    # a dictionary where the selected key defeats the value in the Rock Paper Scissors game.
+    rock_paper_scissors = {'rock': 'scissors', 'scissors': 'paper', 'paper': 'rock'}
+    if rock_paper_scissors[player] == computer:
+        return f"Well done. Computer choose {computer} and failed"
+    if rock_paper_scissors[computer] == player:
+        return f"Sorry, but computer chose {computer}"
+    if player == computer:
+        return f"There is a draw ({player})"
+
+
+choices = ['rock', 'paper', 'scissors']  # list of available choices to play the game
+user_choice = input()  # get user's choice
+computer_choice = random.choice(choices)  # pseudo-random selection of available moves
+winner = winner(user_choice, computer_choice)  # find a winner
+print(winner)  # print winner
